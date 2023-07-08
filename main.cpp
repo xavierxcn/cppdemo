ad7e9f48256e4a5c8192fcd7c905163585d34a9c
xavier <xavier@xavierx.cn>
2023-07-08 10:34:24 +0800

    引用

--- main.cpp
+++ main.cpp
@@ -2,44 +2,34 @@
 
 using namespace std;
 
-void chang(int &v) {
-    v = 1000;
+class Student {
+public:
+    int id;
+    string name;
+    int score;
+
+    void playGame();
+};
+
+void Student::playGame() {
+    cout << "play game" << endl;
+    cout << name << endl;
+
 }
+
 
 int main() {
-    int data = 100;
-    int &ref = data;
+    Student stu;
+    stu.id = 1000;
+    stu.name = "zhangsan";
+    stu.score = 100;
 
-    cout << "data = " << data << endl;
-    chang(data);
-    cout << "data = " << data << endl;
+    cout << stu.id << endl;
 
-    // 对指针的引用
-    int *p = &data;
-    int *&pref = p;
-    cout << "p = " << p << endl;
-    cout << "pref = " << pref << endl;
+    stu.playGame();
 
-    // 对数组的引用
-    int arr[] = {1, 2, 3};
-    int (&refarr)[3] = arr;
-    cout << "arr = " << arr << endl;
-    cout << "refarr = " << refarr << endl;
 
-    string str = "hello";
-    string &refstr = str;
-    cout << "str = " << str << endl;
-    cout << "refstr = " << refstr << endl;
-
-    // 常引用
-    const int &refconst = data;
-    cout << "refconst = " << refconst << endl;
-    // refconst = 1000; // error: assignment of read-only reference ‘refconst’
-    // 只能修改原变量
-    data = 8888;
-    cout << "refconst = " << refconst << endl;
-
-    // 引用的引用
-    int &refref = ref;
-    cout << "refref = " << refref << endl;
+    return 0;
 }
+
+
